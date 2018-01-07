68ac1d89ffbfacdacb9265859aafa0d5748d2cce
liangjinshan <706361560@qq.com>
2018-01-07 21:20:53 +0800

    execl help commit

---
+++ src/main/java/com/execl/help/ExeclHelp.java
@@ -0,0 +1,32 @@
+package com.execl.help;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExeclHelp {
+
+	private static final String pathName = "C:/Users/KIM/Desktop/work_bos/666.xlsx";
+	
+	public static void main(String[] args) throws IOException {
+
+		File file = new File(pathName);
+		InputStream inputStream = new FileInputStream(file);
+		List<ArrayList<String>> list = HostInsertDB.readXlsx(inputStream, file);
+		for (int i = 0; i < list.size(); i++) {
+			//\u00A0 标识空格   Non-Breaking SPace
+			String str = list.get(i).get(0).trim().replace("\u00A0", "");
+			Pattern p = Pattern.compile("\\s*|\t|\r|\n");
+            Matcher m = p.matcher(str);
+            m.replaceAll("");
+			System.out.println("'" +str+ "',");
+		}
+		inputStream.close();
+
+	}
+}
---
+++ src/main/java/com/execl/help/HostInsertDB.java
@@ -0,0 +1,127 @@
+package com.execl.help;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class HostInsertDB {
+
+	
+
+	public static int totalRows; // sheet中总行数
+	public static int totalCells; // 每一行总单元格数
+
+	@SuppressWarnings({ "resource" })
+	public static List<ArrayList<String>> readXlsx(InputStream input, File file) {
+		List<ArrayList<String>> list = new ArrayList<ArrayList<String>>();
+		// IO流读取文件
+		// Workbook workbook ;
+		XSSFWorkbook wb = null;
+		ArrayList<String> rowList = null;
+		try {
+			// 创建文档
+			wb = new XSSFWorkbook(input);
+
+			// 读取sheet(页)
+			for (int numSheet = 0; numSheet < wb.getNumberOfSheets(); numSheet++) {
+				XSSFSheet xssfSheet = wb.getSheetAt(numSheet);
+				if (xssfSheet == null) {
+					continue;
+				}
+				totalRows = xssfSheet.getLastRowNum();
+				// 读取Row,从第二行开始
+				for (int rowNum = 0; rowNum <= totalRows; rowNum++) {
+					XSSFRow xssfRow = xssfSheet.getRow(rowNum);
+					if (xssfRow != null) {
+						rowList = new ArrayList<String>();
+						totalCells = xssfRow.getLastCellNum();
+						// 读取列，从第一列开始
+						for (int c = 0; c <= totalCells + 1; c++) {
+							XSSFCell cell = xssfRow.getCell(c);
+							if (cell == null) {
+								rowList.add(ExcelUtil.EMPTY);
+								continue;
+							}
+							rowList.add(ExcelUtil.getXValue(cell).trim());
+
+						}
+						list.add(rowList);
+					}
+				}
+			}
+			return list;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+
+	}
+
+	@SuppressWarnings({ "resource" })
+	public static List<ArrayList<String>> readXlsx(InputStream input, File file, List<String> interfaces) {
+		List<ArrayList<String>> list = new ArrayList<ArrayList<String>>();
+		// IO流读取文件
+		// Workbook workbook ;
+		XSSFWorkbook wb = null;
+		ArrayList<String> rowList = null;
+		try {
+			// 创建文档
+			wb = new XSSFWorkbook(input);
+
+			// 读取sheet(页)
+			for (int numSheet = 0; numSheet < wb.getNumberOfSheets(); numSheet++) {
+				XSSFSheet xssfSheet = wb.getSheetAt(numSheet);
+				if (xssfSheet == null) {
+					continue;
+				}
+				totalRows = xssfSheet.getLastRowNum();
+				// 读取Row,从第二行开始
+				for (int rowNum = 0; rowNum <= totalRows; rowNum++) {
+					XSSFRow xssfRow = xssfSheet.getRow(rowNum);
+					if (xssfRow != null) {
+						rowList = new ArrayList<String>();
+						totalCells = xssfRow.getLastCellNum();
+						// 读取列，从第一列开始
+						for (int c = 0; c <= totalCells + 1; c++) {
+							XSSFCell cell = xssfRow.getCell(c);
+							if (cell == null) {
+								rowList.add(ExcelUtil.EMPTY);
+								continue;
+							}
+							rowList.add(ExcelUtil.getXValue(cell).trim());
+
+						}
+						list.add(rowList);
+
+					}
+				}
+			}
+			return list;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+
+	}
+
+}
